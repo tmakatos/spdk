e593fb09c24e157c5e532223c31317897f26f9ce
Thanos Makatos <thanos.makatos@nutanix.com>
2020-06-08 06:52:03 -0700

    refactor nvmf_tgt service and fix SELinux context

--- pkg/systemd/usr/local/bin/nvmf_tgt.sh
+++ pkg/systemd/usr/local/bin/nvmf_tgt.sh
@@ -3,16 +3,31 @@
 set -e
 set -x
 
-rm -rf /var/run/muser /dev/shm/muser
-mkdir -p /dev/shm/muser
-chmod 777 /dev/shm/muser
-mkdir -p -m 777 /var/run/muser/{iommu_group,domain}
-chmod 777 /var/run/muser
-rm -f /var/tmp/spdk.sock
+declare -r muser_dir="/var/run/muser"
+
+declare -r spdk_sock="/var/tmp/spdk.sock"
+
+declare -r muser_shm_dir="/dev/shm/muser"
+
+rm -rf ${muser_dir} ${muser_shm_dir}
+mkdir -p ${muser_shm_dir}
+
+# FIXME permissions and context
+mkdir --context=system_u:object_r:tmp_t:s0 -p -m 777 ${muser_dir}/{iommu_group,domain}
+
+# FIXME permissions
+chmod 777 ${muser_shm_dir}
+chmod 777 ${muser_dir}
+
+rm -f ${spdk_sock}
 LD_LIBRARY_PATH=/opt/libiscsi-1.19.0/lib nvmf_tgt -L nvme -L nvmf -L nvmf_muser &
-while [ ! -S /var/tmp/spdk.sock ]; do
+while [ ! -S ${spdk_sock} ]; do
 	sleep 1
 done
-chmod 777 /var/tmp/spdk.sock
+
+# FIXME permissions
+chmod 777 ${spdk_sock}
+chcon -t svirt_tmp_t ${spdk_sock}
+
 /usr/share/spdk/scripts/rpc.py nvmf_create_transport -t MUSER
 wait
