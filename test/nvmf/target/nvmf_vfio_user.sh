90c829aa335b91ea6a9a117d686f0051c17ea488
Thanos Makatos <thanos.makatos@nutanix.com>
2021-04-29 08:19:31 -0400

    FIXME
    Change-Id: Ia7454adcbba3c7c707efa8f1ea13cc0cd4306023

--- test/nvmf/target/nvmf_vfio_user.sh
+++ test/nvmf/target/nvmf_vfio_user.sh
@@ -1,56 +1,62 @@
 #!/usr/bin/env bash
 
+set -x
+
 testdir=$(readlink -f $(dirname $0))
 rootdir=$(readlink -f $testdir/../../..)
 source $rootdir/test/common/autotest_common.sh
 source $rootdir/test/nvmf/common.sh
 
-MALLOC_BDEV_SIZE=64
+MALLOC_BDEV_SIZE=512
 MALLOC_BLOCK_SIZE=512
-NUM_DEVICES=2
 
 rpc_py="$rootdir/scripts/rpc.py"
 
 export TEST_TRANSPORT=VFIOUSER
 
 rm -rf /var/run/muser
+rm -rf /dev/shm/muser
+
+mkdir -p /var/run/muser
+mkdir -p /var/run/muser/iommu_group
+mkdir -p /var/run/muser/domain/muser0/8
+mkdir -p /dev/shm/muser/muser0
 
 # Start the target
-"${NVMF_APP[@]}" -m 0x1 &
+("${NVMF_APP[@]}" -m 0x1 -L vfio_user -L nvmf_vfio -L nvmf_vfio |& tee spdk.log) &
 nvmfpid=$!
 echo "Process pid: $nvmfpid"
 
-trap 'killprocess $nvmfpid; exit 1' SIGINT SIGTERM EXIT
+#trap 'killprocess $nvmfpid; exit 1' SIGINT SIGTERM EXIT
 waitforlisten $nvmfpid
 
 sleep 1
 
 $rpc_py nvmf_create_transport -t VFIOUSER
 
-mkdir -p /var/run/muser
+$rpc_py bdev_malloc_create $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE -b Malloc0
+$rpc_py nvmf_create_subsystem nqn.2019-07.io.spdk:cnode0 -a -s SPDK0
+$rpc_py nvmf_subsystem_add_ns nqn.2019-07.io.spdk:cnode0 Malloc0
+$rpc_py nvmf_subsystem_add_listener nqn.2019-07.io.spdk:cnode0 -t VFIOUSER -a "/var/run/muser/domain/muser0/8" -s 0
 
-for i in $(seq 1 $NUM_DEVICES); do
-	mkdir -p /var/run/muser/domain/muser$i/$i
+ln -s /var/run/muser/domain/muser0/8 /var/run/muser/domain/muser0/8/iommu_group
+ln -s /var/run/muser/domain/muser0/8 /var/run/muser/iommu_group/8
+ln -s /var/run/muser/domain/muser0/8/bar0 /dev/shm/muser/muser0/bar0
 
-	$rpc_py bdev_malloc_create $MALLOC_BDEV_SIZE $MALLOC_BLOCK_SIZE -b Malloc$i
-	$rpc_py nvmf_create_subsystem nqn.2019-07.io.spdk:cnode$i -a -s SPDK$i
-	$rpc_py nvmf_subsystem_add_ns nqn.2019-07.io.spdk:cnode$i Malloc$i
-	$rpc_py nvmf_subsystem_add_listener nqn.2019-07.io.spdk:cnode$i -t VFIOUSER -a "/var/run/muser/domain/muser$i/$i" -s 0
-done
+exit
 
-for i in $(seq 1 $NUM_DEVICES); do
-	$SPDK_EXAMPLE_DIR/identify -r "trtype:VFIOUSER traddr:/var/run/muser/domain/muser$i/$i" -g -L nvme -L nvme_vfio -L vfio_pci
-	sleep 1
-	$SPDK_EXAMPLE_DIR/perf -r "trtype:VFIOUSER traddr:/var/run/muser/domain/muser$i/$i" -s 256 -g -q 128 -o 4096 -w read -t 5 -c 0x2
-	sleep 1
-	$SPDK_EXAMPLE_DIR/perf -r "trtype:VFIOUSER traddr:/var/run/muser/domain/muser$i/$i" -s 256 -g -q 128 -o 4096 -w write -t 5 -c 0x2
-	sleep 1
-	$SPDK_EXAMPLE_DIR/reconnect -r "trtype:VFIOUSER traddr:/var/run/muser/domain/muser$i/$i" -g -q 32 -o 4096 -w randrw -M 50 -t 5 -c 0xE
-	sleep 1
-done
+$SPDK_EXAMPLE_DIR/identify -r 'trtype:VFIOUSER traddr:/var/run/muser/domain/muser0/8' -g -L nvme -L nvme_vfio -L vfio_pci -L nvmf_vfio -L muser
+sleep 1
+$SPDK_EXAMPLE_DIR/perf -r 'trtype:VFIOUSER traddr:/var/run/muser/domain/muser0/8' -s 256 -g -q 128 -o 4096 -w read -t 10 -c 0x2
+sleep 1
+$SPDK_EXAMPLE_DIR/perf -r 'trtype:VFIOUSER traddr:/var/run/muser/domain/muser0/8' -s 256 -g -q 128 -o 4096 -w write -t 10 -c 0x2
+
+$SPDK_EXAMPLE_DIR/reconnect -r 'trtype:VFIOUSER traddr:/var/run/muser/domain/muser0/8' -g -q 32 -o 4096 -w randrw -M 50 -t 10 -c 0xE
+sleep 1
 
 killprocess $nvmfpid
 
 rm -rf /var/run/muser
+rm -rf /dev/shm/muser
 
 trap - SIGINT SIGTERM EXIT
